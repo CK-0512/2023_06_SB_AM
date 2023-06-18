e16796bc10a04c22a50f2d8ad49636cbaf6c06cb
CK-0512 <dhcdkrl1004@gmail.com>
2023-06-18 15:43:54 +0900

    writeArticle

--- src/main/java/com/example/demo/controller/UsrArticleController.java
+++ src/main/java/com/example/demo/controller/UsrArticleController.java
@@ -18,12 +18,21 @@ public class UsrArticleController {
 	public UsrArticleController() {
 		this.lastArticleId = 0;
 		this.articles = new ArrayList<>();
+		
+		makeTestData();
 	}
 
-	@RequestMapping("/usr/home/main")
-	@ResponseBody
-	public Article doAdd(String title, String body) {
-		
+	private void makeTestData() {
+		for(int i = 1; i <= 10; i++) {
+			
+			String title = "제목" + i;
+			String body = "내용" + i;
+			
+			writeArticle(title, body);
+		}
+	}
+	
+	private Article writeArticle(String title, String body) {
 		int id = this.lastArticleId + 1;
 		this.lastArticleId = id;
 		
@@ -33,7 +42,17 @@ public class UsrArticleController {
 		
 		return article;
 	}
+
+	@RequestMapping("/usr/home/main")
+	@ResponseBody
+	public Article doAdd(String title, String body) {
+		
+		Article article = writeArticle(title, body);
+		
+		return article;
+	}
 	
+
 	@RequestMapping("/usr/home/getArticles")
 	@ResponseBody
 	public List<Article> getArticles() {
