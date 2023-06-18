3a19cc790be51f942e5c380067e468dc54c49866
CK-0512 <dhcdkrl1004@gmail.com>
2023-06-18 19:39:28 +0900

    articleService, dao

--- src/main/java/com/example/demo/controller/UsrArticleController.java
+++ src/main/java/com/example/demo/controller/UsrArticleController.java
@@ -1,136 +1,81 @@
 package com.example.demo.controller;
 
-import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.example.demo.service.ArticleService;
 import com.example.demo.vo.Article;
 
 @Controller
 public class UsrArticleController {
 	
-	private int lastArticleId;
-	private List<Article> articles;
+	private ArticleService articleService;
 	
-	public UsrArticleController() {
-		this.lastArticleId = 0;
-		this.articles = new ArrayList<>();
-		
-		makeTestData();
-	}
-
-	private void makeTestData() {
-		for(int i = 1; i <= 10; i++) {
-			
-			String title = "제목" + i;
-			String body = "내용" + i;
-			
-			writeArticle(title, body);
-		}
+	@Autowired
+	public UsrArticleController(ArticleService articleService) {
+		this.articleService = articleService;
 	}
 	
-	// 서비스 메소드
-	private Article writeArticle(String title, String body) {
-		int id = this.lastArticleId + 1;
-		this.lastArticleId = id;
-		
-		Article article = new Article(id, title, body);
-		
-		articles.add(article);
-		
-		return article;
-	}
-	
-	private Article getArticleById(int id) {
-		
-		for (Article article : articles) {
-			if (article.getId() == id) {
-				return article;
-			}
-		}
-		
-		return null;
-	}
-
-	private void deleteArticle(int id) {
-		
-		Article article = getArticleById(id);
-		
-		articles.remove(article);
-	}
-	
-	private void modifyArticle(int id, String title, String body) {
-		
-		Article article = getArticleById(id);
-		
-		article.setTitle(title);
-		article.setBody(body);
-
-	}
-	
-	// 액션 메소드
+	// 액션 메서드
 	@RequestMapping("/usr/article/doAdd")
 	@ResponseBody
 	public Article doAdd(String title, String body) {
 		
-		Article article = writeArticle(title, body);
+		Article article = articleService.writeArticle(title, body);
 		
 		return article;
 	}
-	
 
-	@RequestMapping("/usr/home/getArticles")
+	@RequestMapping("/usr/article/getArticles")
 	@ResponseBody
 	public List<Article> getArticles() {
-		return this.articles;
+		return articleService.getArticles();
 	}
 	
-	@RequestMapping("/usr/home/doDelete")
-	@ResponseBody
-	public String doDelete(int id) {
-		
-		Article foundArticle = getArticleById(id);
-		
-		if (foundArticle == null) {
-			return id + "번 게시글이 존재하지 않습니다";
-		}
-		
-		deleteArticle(id);
-		
-		return id + "번 게시글이 삭제되었습니다.";
-	}
-	
-	@RequestMapping("/usr/home/doModify")
-	@ResponseBody
-	public String doModify(int id, String title, String body) {
-		
-		Article foundArticle = getArticleById(id);
-		
-		if (foundArticle == null) {
-			return id + "번 게시글이 존재하지 않습니다";
-		}
-		
-		modifyArticle(id, title, body);
-		
-		return id + "번 게시글이 수정되었습니다.";
-	}
-	
-	@RequestMapping("/usr/home/getArticle")
+	@RequestMapping("/usr/article/getArticle")
 	@ResponseBody
 	public Object getArticle(int id) {
 		
-		Article foundArticle = getArticleById(id);
+		Article foundArticle = articleService.getArticleById(id);
 		
 		if (foundArticle == null) {
-			return id + "번 게시글이 존재하지 않습니다";
+			return id + "번 게시글은 존재하지 않습니다";
 		}
 		
 		return foundArticle;
 	}
+	
+	@RequestMapping("/usr/article/doModify")
+	@ResponseBody
+	public String doModify(int id, String title, String body) {
+		
+		Article foundArticle = articleService.getArticleById(id);
+		
+		if (foundArticle == null) {
+			return id + "번 게시글은 존재하지 않습니다";
+		}
+		
+		articleService.modifyArticle(id, title, body);
+		
+		return id + "번 게시글을 수정했습니다";
+	}
 
-
-
+	@RequestMapping("/usr/article/doDelete")
+	@ResponseBody
+	public String doDelete(int id) {
+		
+		Article foundArticle = articleService.getArticleById(id);
+		
+		if (foundArticle == null) {
+			return id + "번 게시글은 존재하지 않습니다";
+		}
+		
+		articleService.deleteArticle(id);
+		
+		return id + "번 게시글을 삭제했습니다";
+	}
 }
