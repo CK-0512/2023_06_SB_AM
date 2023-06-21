8e4b35406d99cc76304278aa49cde937f480e008
CK-0512 <dhcdkrl1004@gmail.com>
2023-06-21 21:54:03 +0900

    doAdd, doJoin에 ResultData적용

--- src/main/java/com/example/demo/controller/UsrArticleController.java
+++ src/main/java/com/example/demo/controller/UsrArticleController.java
@@ -25,13 +25,21 @@ public class UsrArticleController {
 	// 액션 메서드
 	@RequestMapping("/usr/article/doAdd")
 	@ResponseBody
-	public Article doAdd(String title, String body) {
+	public ResultData doAdd(String title, String body) {
+		
+		if (Util.empty(title)) {
+			return ResultData.from("F-1", "제목을 입력해주세요");
+		}
+		
+		if (Util.empty(body)) {
+			return ResultData.from("F-2", "내용을 입력해주세요");
+		}
 		
 		articleService.writeArticle(title, body);
 		
 		int id = articleService.getLastInsertId();
 		
-		return articleService.getArticleById(id);
+		return ResultData.from("S-1", Util.f("%d번 게시글이 생성되었습니다", id), articleService.getArticleById(id));
 	}
 
 	@RequestMapping("/usr/article/getArticles")
@@ -50,7 +58,7 @@ public class UsrArticleController {
 			return ResultData.from("F-1", Util.f("%d번 게시글은 존재하지 않습니다", id));
 		}
 		
-		return ResultData.from("S-1", Util.f("%d번 게시글 입니다.", id), foundArticle);
+		return ResultData.from("S-1", Util.f("%d번 게시글 입니다", id), foundArticle);
 	}
 	
 	@RequestMapping("/usr/article/doModify")
