ec7042bdb4b1cb4790cdc4c007b37fb6ee1b3727
CK-0512 <dhcdkrl1004@gmail.com>
2023-06-27 21:00:53 +0900

    detail design, list detail에 writerName

--- src/main/java/com/example/demo/controller/UsrArticleController.java
+++ src/main/java/com/example/demo/controller/UsrArticleController.java
@@ -62,7 +62,7 @@ public class UsrArticleController {
 	@RequestMapping("/usr/article/detail")
 	public String showDetail(Model model, int id) {
 
-		Article foundArticle = articleService.getArticleById(id);
+		Article foundArticle = articleService.getForPrintArticle(id);
 		
 		model.addAttribute("article", foundArticle);
 		
