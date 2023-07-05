0c198216e2e47a71d0e3f0f285f2cfa9e180030c
CK-0512 <dhcdkrl1004@gmail.com>
2023-07-05 20:49:48 +0900

    list에 검색 기능

--- src/main/java/com/example/demo/service/ArticleService.java
+++ src/main/java/com/example/demo/service/ArticleService.java
@@ -10,36 +10,36 @@ import com.example.demo.vo.Article;
 
 @Service
 public class ArticleService {
-
+	
 	private ArticleDao articleDao;
-
+	
 	@Autowired
-	ArticleService(ArticleDao articleDao) {
+	ArticleService(ArticleDao articleDao){
 		this.articleDao = articleDao;
 	}
-
+	
 	// 서비스 메서드
 	public void writeArticle(int memberId, int boardId, String title, String body) {
 		articleDao.writeArticle(memberId, boardId, title, body);
 	}
-
+	
 	public Article getArticleById(int id) {
 		return articleDao.getArticleById(id);
 	}
-
+	
 	public void modifyArticle(int id, String title, String body) {
 		articleDao.modifyArticle(id, title, body);
 	}
-
+	
 	public void deleteArticle(int id) {
 		articleDao.deleteArticle(id);
 	}
 
-	public List<Article> getArticles(int boardId, int itemsInAPage, int page) {
-
+	public List<Article> getArticles(int boardId, String searchKeyword, String searchKeywordType, int itemsInAPage, int page) {
+		
 		int limitStart = (page - 1) * itemsInAPage;
-
-		return articleDao.getArticles(boardId, limitStart, itemsInAPage);
+		
+		return articleDao.getArticles(boardId, searchKeyword, searchKeywordType, limitStart, itemsInAPage);
 	}
 
 	public int getLastInsertId() {
@@ -50,8 +50,8 @@ public class ArticleService {
 		return articleDao.getForPrintArticle(id);
 	}
 
-	public int getArticlesCnt(int boardId) {
-		return articleDao.getArticlesCnt(boardId);
+	public int getArticlesCnt(int boardId, String searchKeyword, String searchKeywordType) {
+		return articleDao.getArticlesCnt(boardId, searchKeyword, searchKeywordType);
 	}
-
+	
 }
