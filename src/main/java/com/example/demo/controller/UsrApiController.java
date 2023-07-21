918a4170b1b534f7a4d4daf244a12c51f2d758ce
CK-0512 <dhcdkrl1004@gmail.com>
2023-07-21 23:56:03 +0900

    APITest, 카카오맵api

---
+++ src/main/java/com/example/demo/controller/UsrApiController.java
@@ -0,0 +1,18 @@
+package com.example.demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class UsrApiController {
+	
+	@RequestMapping("/usr/api/APITest")
+	public String APITest() {
+		return "usr/api/APITest";
+	}
+	
+	@RequestMapping("/usr/api/APITest2")
+	public String APITest2() {
+		return "usr/api/APITest2";
+	}
+}
